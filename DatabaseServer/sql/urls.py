6eab1b220149ca47405e64b433349132f7879b56
Felix <fedecraft3312@gmail.com>
2023-01-23 01:09:38 -0300

    Added route getRepository again

--- DatabaseServer/sql/urls.py
+++ DatabaseServer/sql/urls.py
@@ -7,27 +7,15 @@ https://docs.djangoproject.com/en/4.1/intro/tutorial01/
 import os
 import pprint
 
-from django.urls import path
-
-from . import views
-from .views import RepositorySettings
 from src.submodules.dev_tools_utils.dynamic_imports.django_routes import DjangoRoutes
 
 app_name = "sql"
-# urlpatterns = [
-#     # ex: /sql/
-#     path("", views.index, name="index"),
-#     # ex: /sql/RepositorySettings/getAll
-#     path("RepositorySettings/getAll/", RepositorySettings.get_all, name="RepositorySettings/getAll/"),
-#     path("RepositorySettings/discoverAndReset/",
-#          RepositorySettings.discover_and_reset,
-#          name="RepositorySettings/discoverAndReset/"),
-#     path("RepositorySettings/getRepository/",
-#          RepositorySettings.get_repository,
-#          name="RepositorySettings/getRepository/"),
-# ]
+
 routes_path = f"{os.getcwd()}{os.path.sep}src{os.path.sep}routes"
-djroutes = DjangoRoutes(routes_path, handle_request=False)
+djroutes = DjangoRoutes(routes_path, handle_request=False, debug=True)
 urlpatterns = djroutes.get_routes_as_urlpatterns()
-print("Urlpatterns:")
-pprint.pprint(urlpatterns)
+
+debug = False
+if debug:
+    print("Urlpatterns:")
+    pprint.pprint(urlpatterns)
